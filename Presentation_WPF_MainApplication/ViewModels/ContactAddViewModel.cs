a3d81fc97a7d6ec604443d7a2ca6ff4e13836c24
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2025-01-06 11:53:51 +0100

    I have configured the add contact functionality to work in the WPF application. When a user clicks save, the AddContact method is triggered, which in turn calls SaveContact. This ensures that the contact is now correctly saved to the JSON file.

--- Presentation_WPF_MainApplication/ViewModels/ContactAddViewModel.cs
+++ Presentation_WPF_MainApplication/ViewModels/ContactAddViewModel.cs
@@ -1,10 +1,37 @@
-﻿
+﻿using Business.Interfaces;
+using Business.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Presentation_WPF_MainApplication.ViewModels;
 
 public partial class ContactAddViewModel : ObservableObject
 {
+    private readonly IContactService _contactService;
+    private readonly IServiceProvider _serviceProvider;
+
     [ObservableProperty]
-    private string _title = "Add new Contact";
+    private ContactRegistrationForm _contact = new();
+
+    public ContactAddViewModel(IContactService contactService, IServiceProvider serviceProvider)
+    {
+        _contactService = contactService;
+        _serviceProvider = serviceProvider;
+    }
+
+    [RelayCommand]
+    private void Save()
+    {
+        _contactService.AddContact(Contact);
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        mainViewModel.CurrentViewModel = _serviceProvider.GetRequiredService<ContactListViewModel>();
+    }
+
+    [RelayCommand]
+    private void Cancel()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        mainViewModel.CurrentViewModel = _serviceProvider.GetRequiredService<ContactListViewModel>();
+    }
 }
